effcbb6a115a9d0bf61c8ac05082568972981570
Cachiu <huyhuy9126@gmail.com>
2024-11-27 23:50:07 +0700

    Add files via upload

--- ss7_bt1.c
+++ ss7_bt1.c
@@ -2,8 +2,11 @@
 
 int main(int argc, const char * argv[]) {
     int a[6]={1,2,3,4,5,6};
+    int n=0;
     for(int i = 0;i<6;i++){
         printf("%d\n",a[i]);
+        n++;
     }
+    printf("do dai mang la %d", n);
     return 0;
 }
